8220a22ecb9585f34a054c0e26f716055f875200
drsheep <yangjie0825@me.com>
2025-12-20 11:37:40 +0800

    add hydra

--- run.py
+++ run.py
@@ -1,34 +1,38 @@
 import os
 import pandas as pd
+import hydra
+
 from dotenv import load_dotenv
+from omegaconf import DictConfig
 
 from src.config import MomentumConfig
 from src.data import get_catalog, get_top_liquid_instruments, create_data_configs
 from src.engine import run_backtest
 
 # Load environment
-load_dotenv()
-NAUTILUS_ROOT = os.getenv('NAUTILUS_ROOT')
 
-def main():
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg: DictConfig):
     # 1. Prepare Data
+    load_dotenv()
+    NAUTILUS_ROOT = os.getenv('NAUTILUS_ROOT')
     catalog = get_catalog(NAUTILUS_ROOT)
-    instruments = get_top_liquid_instruments(catalog, limit=10)
+    instruments = get_top_liquid_instruments(catalog,
+                                             limit=cfg.universe.top_n_instruments)
     print(f"Selected Instruments: {instruments}")
-
     data_configs = create_data_configs(NAUTILUS_ROOT, instruments)
 
     # 2. Define Parameters
-    start_date = pd.Timestamp('2024-10-01', tz='UTC')
-    end_date = pd.Timestamp('2024-11-01', tz='UTC')
-    venue = "XNYS"
-    starting_balances=["1_000_000 USD"]
-
+    start_date = pd.Timestamp(cfg.backtest.start_date, tz='UTC')
+    end_date = pd.Timestamp(cfg.backtest.end_date, tz='UTC')
+    venue = cfg.backtest.venue
     strategy_config = MomentumConfig(
         instrument_ids=instruments,
         venue=venue,
     )
 
+    starting_balances = list(cfg.backtest.starting_balances)
+    print(start_date, end_date, venue, starting_balances)
     # 3. Run
     results = run_backtest(
         strategy_path="src.strategy:MomentumStrategy",
